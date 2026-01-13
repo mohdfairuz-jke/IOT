1b0d0e3b6f760bd8048b9dddc32ce05618e078f6
mohdfairuz-jke <mohdfairuzsalleh99@gmail.com>
2026-01-13 12:48:22 +0800

    Update Ultrasonic Sensor .ino

--- Ultrasonic Sensor .ino
+++ Ultrasonic Sensor .ino
@@ -1,12 +1,12 @@
 //Name:
 //Reg.Num:
-const int echoPin = 2; // Echo Pin of Ultrasonic Sensor 
-constintpingPin=3;     // Trigger Pin of Ultrasonic Sensor 
+const int echoPin = 2;     // Echo Pin of Ultrasonic Sensor 
+const int pingPin = 3;     // Trigger Pin of Ultrasonic Sensor 
 
 void setup()
 {
 Serial.begin(9600);       // Starting Serial Communication 
-pinMode(pingPin,OUTPUT);  //initialisingpin3asoutput 
+pinMode(pingPin,OUTPUT);  // initialising pin 3 as output 
 pinMode(echoPin, INPUT);  // initialising pin 2 as input
 }
 
@@ -18,9 +18,12 @@ delayMicroseconds(2);
 digitalWrite(pingPin,HIGH); 
 delayMicroseconds(10); 
 digitalWrite(pingPin,LOW);
-duration=pulseIn(echoPin,HIGH);           //usingpulsinfunctiontodeterminetotaltime 
-inches = microsecondsToInches(duration);  // calling method
-cm=microsecondsToCentimeters(duration);   //callingmethod 
+  
+duration=pulseIn(echoPin,HIGH);           //using pulse in functin onto determine total time, read reflected sound wave
+  
+inches = microsecondsToInches(duration);  // convert the time into a distances
+cm=microsecondsToCentimeters(duration);   // convert the time into a distances 
+  
 Serial.print(inches);
 Serial.print("in,"); 
 Serial.print(cm); 
@@ -28,11 +31,11 @@ Serial.print("cm");
 Serial.println(); 
 delay(100);
 }
-longmicrosecondsToInches(longmicroseconds) //methodtocovertmicrosectoinches
+longmicrosecondsToInches(longmicroseconds) //method function to conert into inches
 {
-returnmicroseconds/74/2;
+returnmicroseconds/74/2;  // from datasheet for the PING, there are 73.746 microseconds per inch
 }
-longmicrosecondsToCentimeters(longmicroseconds)   //methodtocovertmicrosectocentimeters
+longmicrosecondsToCentimeters(longmicroseconds)   //method function to covert into centimeters
 {
-returnmicroseconds/29/2;
+returnmicroseconds/29/2;  //The speed of sound is 29 microseconds per centimeter. 
 }
