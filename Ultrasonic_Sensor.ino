1f397b10ba9cf8baf8e3c12cd53d40e4b3994aba
mohdfairuz-jke <mohdfairuzsalleh99@gmail.com>
2026-02-06 09:33:21 +0800

    Refactor Ultrasonic Sensor code and fix pin assignments

--- Ultrasonic_Sensor.ino
+++ Ultrasonic_Sensor.ino
@@ -1,41 +1,51 @@
-//Name:
-//Reg.Num:
-const int echoPin = 2;     // Echo Pin of Ultrasonic Sensor 
-const int pingPin = 3;     // Trigger Pin of Ultrasonic Sensor 
+// Name:
+// Reg.Num:
 
-void setup()
-{
-Serial.begin(9600);       // Starting Serial Communication 
-pinMode(pingPin,OUTPUT);  // initialising pin 3 as output 
-pinMode(echoPin, INPUT);  // initialising pin 2 as input
+const int echoPin = 2;     // Echo Pin of Ultrasonic Sensor
+const int pingPin = 4;     // Trigger Pin of Ultrasonic Sensor
+
+void setup() {
+  Serial.begin(9600);      	// Starting Serial Communication
+  pinMode(pingPin, OUTPUT); 	// initialising pin 4 as output
+  pinMode(echoPin, INPUT);  	// initialising pin 2 as input
 }
 
-voidloop()
-{
-long duration, inches, cm; 
-digitalWrite(pingPin,LOW); 
-delayMicroseconds(2); 
-digitalWrite(pingPin,HIGH); 
-delayMicroseconds(10); 
-digitalWrite(pingPin,LOW);
+void loop() {
+  long duration, inches, cm; 
+
+  // Menghantar signal trigger
+  digitalWrite(pingPin, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(pingPin, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(pingPin, LOW);
   
-duration=pulseIn(echoPin,HIGH);           //using pulse in functin onto determine total time, read reflected sound wave
+  // Membaca tempoh pantulan ultrasound (gelombang bunyi)
+  duration = pulseIn(echoPin, HIGH); 
   
-inches = microsecondsToInches(duration);  // convert the time into a distances
-cm=microsecondsToCentimeters(duration);   // convert the time into a distances 
+  // Mengubah waktu menjadi jarak
+  inches = microsecondsToInches(duration); 
+  cm = microsecondsToCentimeters(duration); 
   
-Serial.print(inches);
-Serial.print("in,"); 
-Serial.print(cm); 
-Serial.print("cm"); 
-Serial.println(); 
-delay(100);
+  // Menampilkan hasil ke Serial Monitor
+  Serial.print(inches);
+  Serial.print(" in, "); 
+  Serial.print(cm); 
+  Serial.println(" cm"); 
+  
+  delay(100);
 }
-longmicrosecondsToInches(longmicroseconds) //method function to conert into inches
-{
-returnmicroseconds/74/2;  // from datasheet for the PING, there are 73.746 microseconds per inch
+
+// Fungsi pertukaran ke Inci
+long microsecondsToInches(long microseconds) {
+  // Ada spasi antara 'return' dan variable
+  return microseconds / 74 / 2;     
+  // from datasheet for the PING, there are 73.746 microseconds per inch
 }
-longmicrosecondsToCentimeters(longmicroseconds)   //method function to covert into centimeters
-{
-returnmicroseconds/29/2;  //The speed of sound is 29 microseconds per centimeter. 
+
+// Fungsi pertukaran ke Centimeter
+long microsecondsToCentimeters(long microseconds) {
+  // Ada space antara 'return' dan variable
+  return microseconds / 29 / 2;      
+//The speed of sound is 29 microseconds per centimeter.
 }
